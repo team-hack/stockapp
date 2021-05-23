b446629d150cebe5ecf04cfe7f7b3d8320428354
Vijay Menon <vijaymenonx@gmail.com>
2021-05-23 11:49:42 -0700

    Check changes to components

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // Pages
 import { Page } from "./components/Page";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -42,6 +42,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
