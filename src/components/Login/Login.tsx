54eb56ea9ae3675872a87c2a51f38b23cf714a1f
Joey <35553162+javascriptjoey@users.noreply.github.com>
2021-06-09 14:07:58 -0700

    My first commit to the project created Login component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LoginMain } from "./LoginStyled"
+
+const Login = ( ): JSX.Element => {
+  return (
+    <LoginMain>THIS IS A TEST</LoginMain>
+  );
+};
+
+export default Login;
---
+++ src/components/Login/LoginStyled.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+export const LoginMain = styled.h1`
+background-color:yellow;
+`;
