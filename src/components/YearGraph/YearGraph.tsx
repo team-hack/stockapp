a48c4c6ac4dedced5f7a3fdbdf76e5b0c8a4f874
Joey <35553162+javascriptjoey@users.noreply.github.com>
2021-07-01 11:41:46 -0700

    Updated YearGraph Component-- Progress updates

--- src/components/YearGraph/YearGraph.tsx
+++ src/components/YearGraph/YearGraph.tsx
@@ -12,6 +12,18 @@ import { LabelFour } from "./YearGraphStyled";
 import { LabelFive } from "./YearGraphStyled";
 import { LabelSix } from "./YearGraphStyled";
 import { LabelSeven } from "./YearGraphStyled";
+// ThirdContainer Imported
+import { ThirdContainer } from "./YearGraphStyled";
+import { ButtonOne } from "./YearGraphStyled";
+import { ButtonTwo } from "./YearGraphStyled";
+import { ButtonThree } from "./YearGraphStyled";
+import { ButtonFour } from "./YearGraphStyled";
+import { ButtonFive } from "./YearGraphStyled";
+import { ButtonSix } from "./YearGraphStyled";
+// FourthContainer Imported
+import { FourthContainer } from "./YearGraphStyled";
+import { StockChartPlaceholder } from "./YearGraphStyled";
+import { PlaceholderMessage } from "./YearGraphStyled";
 // Creating a YearGraph JSX Element 
 const YearGraph = ( ): JSX.Element => {
   return (
@@ -22,11 +34,24 @@ const YearGraph = ( ): JSX.Element => {
           <LabelThree>0.10(0.53%)</LabelThree>
         </MainContainer>
         <SecondContainer>
-          <LabelFour>19.33</LabelFour>
+        <LabelFour>19.33</LabelFour>
           <LabelFive>  /\  </LabelFive>
           <LabelSix>0.33(1.74%)</LabelSix>
           <LabelSeven>After Hours . June 29, 7:59PM EDT . Market Closed </LabelSeven>
         </SecondContainer>
+        <ThirdContainer>
+          <ButtonOne>Day</ButtonOne>
+          <ButtonTwo>Week</ButtonTwo>
+          <ButtonThree>Month</ButtonThree>
+          <ButtonFour>Year</ButtonFour>
+          <ButtonFive>5 Year</ButtonFive>
+          <ButtonSix>Max</ButtonSix>
+        </ThirdContainer>
+        <FourthContainer>
+          <StockChartPlaceholder>
+            <PlaceholderMessage>I am a really cool 3d stock chart and this is my temporary placeholder I update in realtime and show you the Money!</PlaceholderMessage>
+          </StockChartPlaceholder>
+        </FourthContainer>
       </Wrapper>
   );
 };
--- src/components/YearGraph/YearGraphStyled.tsx
+++ src/components/YearGraph/YearGraphStyled.tsx
@@ -5,23 +5,27 @@ import styled from "styled-components";
 // Wrapper that goes around the First and Second Container
 export const Wrapper = styled.div`
 background-color: rgb(240, 239, 239);
-height: 100vh;
+padding-left:50px;
+padding-right: 50px;
+padding-bottom: 40px;
+margin-top: 50px;
+//height: 100vh;
+margin-left: 60px;
+margin-right: 60px;
 padding-top: 40px;
+border-radius: 10px;
+border-color: rgb(255, 0, 0);
+border-style: solid;
 `
 
 // First Container aka Main Container
 export const MainContainer = styled.div`
+padding-left: 10px;
+padding-top: 10px;
+padding-bottom: 10px;
 background-color: white;
-margin-left: 20px;
-margin-right: 20px;
-padding-top: 20px;
-padding-left: 20px;
-height: 100px;
-border-radius: 10px;
-border-color: black;
-border-style: solid;
--webkit-box-shadow: 5px 5px 15px 5px #000000; 
-box-shadow: 5px 5px 15px 5px #000000;
+display: flex;   
+align-items: flex-end;
 `
 // First Container- Label 1:
 export const LabelOne = styled.label`
@@ -41,23 +45,16 @@ background-color: beige;
 font-size: 30px;
 font-weight: 700;
 color: green;
+margin-right: 10px;
 `// -End- First Container-->
 
 
 // Second Container 
 export const SecondContainer = styled.div`
+padding-left: 10px;
 background-color: white;
-margin-top: 40px;
-margin-left: 20px;
-margin-right: 20px;
-padding-top: 20px;
-padding-left: 20px;
-height: 100px;
-border-radius: 10px;
-border-color: black;
-border-style: solid;
--webkit-box-shadow: 5px 5px 15px 5px #000000; 
-box-shadow: 5px 5px 15px 5px #000000;
+display: flex;
+flex-direction: row;
 `
 // Second Container- Label 4:
 export const LabelFour = styled.label`
@@ -66,11 +63,13 @@ margin-right: 10px;
 color: grey;
 font-size: 25px;
 font-weight: 700;
+align-items:center;
 `
 // Second Container- Label 5:
 export const LabelFive = styled.label`
 background-color: beige;
 margin-right: 10px;
+align-items:center;
 `
 // Second Container- Label 6:
 export const LabelSix = styled.label`
@@ -79,6 +78,7 @@ margin-right: 10px;
 font-size: 25px;
 font-weight: 700;
 color: green;
+align-items:center;
 `
 // Second Container- Label 7:
 export const LabelSeven = styled.label`
@@ -86,5 +86,91 @@ background-color: beige;
 color: grey;
 font-size: 20px;
 font-weight: 700;
+align-items:center;
 `// -End- Second Container-->  
  
+
+// Third Container 
+export const ThirdContainer = styled.div`
+padding-left: 10px;
+background-color: white;
+display: flex;
+flex-direction: row;
+padding-top: 10px;
+padding-bottom: 10px;
+align-self: flex-start;
+`
+// Third Container- Button 1:
+export const ButtonOne = styled.button`
+margin-right: 20px;
+width: 150px;
+height: 40px;
+border-radius: 40px;
+color: blue;
+`
+// Third Container- Button 2:
+export const ButtonTwo = styled.button`
+margin-right: 20px;
+width: 150px;
+border-radius: 40px;
+color: blue;
+`
+// Third Container- Button 3:
+export const ButtonThree = styled.button`
+margin-right: 20px;
+width: 150px;
+border-radius: 40px;
+color: blue;
+`
+// Third Container- Button 4:
+export const ButtonFour = styled.button`
+margin-right: 20px;
+width: 150px;
+border-radius: 40px;
+color: blue;
+`
+// Third Container- Button 5:
+export const ButtonFive = styled.button`
+margin-right: 20px;
+width: 150px;
+border-radius: 40px;
+color: blue;
+`
+// Third Container- Button 6:
+export const ButtonSix = styled.button`
+margin-right: 20px;
+width: 150px;
+border-radius: 40px;
+color: blue;
+`// -End- Third Container-->  
+
+
+// Fourth Container 
+export const FourthContainer = styled.div`
+padding-left: 10px;
+padding-right: 10px;
+background-color: white;
+display: flex;
+flex-direction: row;
+padding-top: 10px;
+padding-bottom: 10px;
+align-items: center;
+border-style: solid;
+border-color: orange;
+`
+// Fourth Container- StockChartPlaceholder
+export const StockChartPlaceholder = styled.div`
+height: 400px;
+width: 100vw;
+background-color: green;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+// Fourth Container- PlaceholderMessage
+export const PlaceholderMessage = styled.p`
+font-size: 20px;
+font-weight: bold;
+color:white;
+padding-left: 10px;
+`
