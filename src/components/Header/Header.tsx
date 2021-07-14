80319ee3621924d1eab983ad6bbbcc4fdef32d10
Amy Dang <kirontoo@gmail.com>
2021-07-13 20:51:40 -0700

    Add link to sign out user

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { HeaderMain, RoundedButton } from "./HeaderStyled";
+import { useAuth } from "../../context/AuthProvider";
 
 import "./header.styles.css";
 
@@ -8,7 +9,7 @@ import "./header.styles.css";
 
 // }
 
-const Header = (): JSX.Element => {
+const Header = () => {
   const data = [
     { id: 1, title: "Day" },
     { id: 2, title: "Week" },
@@ -16,6 +17,30 @@ const Header = (): JSX.Element => {
     { id: 4, title: "Year" },
     { id: 5, title: "Five Year" },
   ];
+
+  const auth = useAuth();
+
+  const logoutUser = () => {
+    auth.logout();
+  }
+
+  const generateNavLinks = () => {
+    if (auth.user?.authenticated)  {
+      return (<a onClick={logoutUser}>Sign out</a>)
+    }
+
+    return (
+      <>
+        <Link className="login" to="/login">
+        Login
+        </Link>
+        <Link className="signup" to="/">
+        Signup
+        </Link>
+      </>
+    );
+  };
+
   return (
     <HeaderMain dark={true}>
       <div className="left">
@@ -32,12 +57,7 @@ const Header = (): JSX.Element => {
         <Link to="/protected">
           Protected
         </Link>
-        <Link className="login" to="/login">
-          Login
-        </Link>
-        <Link className="signup" to="/signup">
-          Signup
-        </Link>
+        {generateNavLinks()}
       </div>
     </HeaderMain>
   );
