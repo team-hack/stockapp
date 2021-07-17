7a00c515ee803ec43a67589b8951b53606e0ebda
Joey <35553162+javascriptjoey@users.noreply.github.com>
2021-07-17 16:56:01 -0700

    Added container three buttons

--- src/components/YearGraph/YearGraph.tsx
+++ src/components/YearGraph/YearGraph.tsx
@@ -6,31 +6,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretUp } from '@fortawesome/free-solid-svg-icons'
 import { faStar } from '@fortawesome/free-regular-svg-icons'
 import { Label } from "./YearGraphStyled";
+import { Button } from "./YearGraphStyled";
 
 
 // Creating a YearGraph JSX Element 
 const YearGraph = ( ): JSX.Element => {
-  
+// Container 1   
   const containerOneLabels = [
-    {id:1, value: " 19.00", labelName:"currentPriceLabel"},
+    {id:1, value: "19.00", labelName:"currentPriceLabel"},
     {id:2, value: <FontAwesomeIcon id="icon" icon={faCaretUp}/> , labelName:"faIconLabel"},
     {id:3, value: " 0.10", labelName:"priceDiffernceLabel" },
     {id:4, value: " (00.53%)", labelName:"priceDifferncePercentLabel"},
   ];
-  
+
+// Container 2
+
+
+// Container 3 
+const containerThreeButtons = [
+    {id:5, title: "Day"},
+    {id:6, title: "Week"},
+    {id:7, title: "Month"},
+    {id:8, title: "Year"},
+    {id:9, title: "5 Year"},
+    {id:10,title: "Max"},
+];  
   
   return (
    <div className="wrapper">
-      <div className="container-1">;
+      <div className="container-1">
           { containerOneLabels.map((containerOneLabels)=>(
             <Label id={ `${containerOneLabels.id}` } labelName={containerOneLabels.labelName}> {containerOneLabels.value} </Label>))
           }  
       </div>
+
       <div className="container-2">
-        Buttons go here and will be Dynamically Loaded by react
+         Some more Labels go here
       </div>
+
       <div className="container-3">
-        Labels go here
+         { containerThreeButtons.map((containerThreeButtons)=>(
+            <Button id={ `${containerThreeButtons.id}` }> {containerThreeButtons.title} </Button>))
+         }
       </div>
       <div className="container-4">
         3D Chart goes here
--- src/components/YearGraph/YearGraphStyled.tsx
+++ src/components/YearGraph/YearGraphStyled.tsx
@@ -8,44 +8,47 @@ min-height: 100vh;
 flex-direction: column;
 background-color:#eceff1;
 `
-
-
-
+export const Button = styled.button`
+width: 90px;
+height: 30px;
+border-radius: 25px;
+margin-right: 20px;
+color: #636363;
+&:hover {
+  background: #009688;
+  color: white;
+  cursor: pointer;
+}
+`
 
 export const Label =  styled.label <{ labelName: string }>`
+  display: flex;
+  font-size: 20px;
+  color: grey;
+  padding-right:4px;
+    ${props => props.labelName == "currentPriceLabel" && css`
+        color: black;
+        font-size: 2em;
+        padding-left:10px;
+      `   
+      }
 
-color: green;
+    ${props => props.labelName == "faIconLabel" && css`
+        align-items: flex-end;
+        color: green;
+        font-size: 20px;
+      `   
+      }  
 
+    ${props => props.labelName == "priceDiffernceLabel" && css` 
+        color: green;
+        font-size: 15px;
+    `
+    }
 
-${props => props.labelName == "currentPriceLabel" && css`
-     color: #fbb800;
-   
-   `   
-  }
-
-
-${props => props.labelName == "faIconLabel" && css`
-    color: #2121a4;
-    
-   `   
-  }  
-
-
-${props => props.labelName == "priceDiffernceLabel" && css` 
-    color: red;
- 
- `
-}
-
-
-${props => props.labelName == "priceDifferncePercentLabel" && css` 
-    color: aquamarine;
- 
- `
-}
-
-
-
-
-font-size: 40px;
+    ${props => props.labelName == "priceDifferncePercentLabel" && css` 
+        color: green;
+        font-size: 15px;
+    `
+    }
 `
